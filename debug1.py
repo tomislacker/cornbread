489c77d3bbd3a9e0e14578f4371870042e2d04d1
Ben Tomasik <ben@tomasik.io>
2017-06-07 17:41:00 -0500

    Add another debug script

---
+++ debug1.py
@@ -0,0 +1,22 @@
+import logging
+import threading
+from cornbread.xorg import *
+
+if __name__ == '__main__':
+    logging.warning('Creating FW')
+    w = FocusedWindow()
+
+    logging.warning('Creating FW thread')
+    t = threading.Thread(target=FocusedWindowWatcher, args=(w,))
+
+    logging.warning('Starting thread')
+    t.start()
+
+    try:
+        logging.warning('Joining FW thread')
+        t.join(4)
+
+    except KeyboardInterrupt as e:
+        logging.warning('Keyboard interrupt')
+        w._exit_watch = True
+        t.join(4)
